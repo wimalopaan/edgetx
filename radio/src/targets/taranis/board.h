6d70790af81e5658c0e79f7ebeb956c9caded13e
Raphael Coeffic <raphael.coeffic@frafos.com>
2021-11-21 08:44:51 +0100

    Fixed WDG and some others for gtests & simu

--- radio/src/targets/taranis/board.h
+++ radio/src/targets/taranis/board.h
@@ -28,6 +28,8 @@
 #include "board_common.h"
 #include "hal.h"
 
+#include "watchdog_driver.h"
+
 #if defined(RADIO_TX12) || defined(RADIO_TX12)
   #define  NAVIGATION_X7_TX12
 #endif
@@ -473,20 +475,6 @@ uint32_t readKeys();
 uint32_t readTrims();
 #define TRIMS_PRESSED()                 (readTrims())
 #define KEYS_PRESSED()                  (readKeys())
-
-// WDT driver
-#define WDG_DURATION                      500 /*ms*/
-#if !defined(WATCHDOG) || defined(SIMU)
-  #define WDG_ENABLE(x)
-  #define WDG_RESET()
-#else
-  #define WDG_ENABLE(x)                 watchdogInit(x)
-  #define WDG_RESET()                   IWDG->KR = 0xAAAA
-#endif
-void watchdogInit(unsigned int duration);
-#define WAS_RESET_BY_SOFTWARE()             (RCC->CSR & RCC_CSR_SFTRSTF)
-#define WAS_RESET_BY_WATCHDOG()             (RCC->CSR & (RCC_CSR_WDGRSTF | RCC_CSR_WWDGRSTF))
-#define WAS_RESET_BY_WATCHDOG_OR_SOFTWARE() (RCC->CSR & (RCC_CSR_WDGRSTF | RCC_CSR_WWDGRSTF | RCC_CSR_SFTRSTF))
 
 // ADC driver
 enum Analogs {
