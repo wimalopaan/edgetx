cf1ac241e041be1c085fe5f2f0d5603f55c53575
wimalopaan <wilhelm.wm.meier@googlemail.com>
2025-10-09 20:50:12 +0200

    typo

--- compile_firmware.sh
+++ compile_firmware.sh
@@ -3,8 +3,8 @@
 # auch in der shell zum compilieren, sonst im Simulator Fehler bei Zahlliteralen wie 0.1
 export LANG=C
 
-make submodule init
-make submodule update
+git submodule init
+git submodule update
 make configure
 
 make -C arm-none-eabi -j2 firmware
